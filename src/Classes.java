cb66741cf469e1306ba8c22c286c4c9779ca3863
Manuel Alejandro Rodriguez Bogota <manuel.rodriguez02@globant.com>
2026-08-20 11:40:37 -0500

    Encapsulamiento Classes y Student, agregar lista de estudiantes con addStuden/getStudents

--- src/Classes.java
+++ src/Classes.java
@@ -1,11 +1,34 @@
-public class Classes {
-    String name;
-    Integer classroom;
-    String teacher;
+import java.util.ArrayList;
 
-    public Classes(String name, Integer classroom, String teacher){
+public class Classes {
+    private String name;
+    private Integer classroom;
+    private Teacher teacher;
+    private ArrayList<Student> studentsList= new ArrayList<>();
+
+    public Classes(String name, Integer classroom, Teacher teacher){
         this.name = name;
         this.classroom = classroom;
         this.teacher = teacher;
+    }
+
+    public void addStudent(Student student){
+        studentsList.add(student);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Integer getClassroom(){
+        return this.classroom;
+    }
+
+    public Teacher getTeacher(){
+        return this.teacher;
+    }
+
+    public ArrayList<Student> getStudents(){
+        return this.studentsList;
     }
 }
